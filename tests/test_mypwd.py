72bde2d14f9e85b01f61bba98e5cb64a58ad881e
Jaroslav Beran <jaroslav.beran@gmail.com>
2021-10-05 22:57:02 +0200

    Test refa

--- tests/test_mypwd.py
+++ tests/test_mypwd.py
@@ -1,27 +1,38 @@
 import mypwd
-import unittest
 import os
+import pytest
 
 
-class TestMyPwd(unittest.TestCase):
-
-    def testMyPwd(self):
+@pytest.fixture(autouse=True, scope="module")
+def setup():
+    try:
         mypwd.filename = "mypwd_test.json"
-        self.assertEqual("john", mypwd.get_login("postgres"))
-        self.assertEqual("myPa$$w0rd", mypwd.get_pwd("postgres"))
-        self.assertEqual("Valid until end of month", mypwd.get_value("postgres", "note"))
-
-        login, password = mypwd.get_login_password("postgres")
-        self.assertEqual("john", login)
-        self.assertEqual("myPa$$w0rd", password)
-
-        login, password, note = mypwd.get_values("postgres", ["login", "password", "note"])
-        self.assertEqual("john", login)
-        self.assertEqual("myPa$$w0rd", password)
-        self.assertEqual("Valid until end of month", note)
-
+        yield mypwd
+    finally:
         os.remove(mypwd.__get_path__())
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_login():
+    assert "john" == mypwd.get_login("postgres")
+
+
+def test_pwd():
+    assert "myPa$$w0rd" == mypwd.get_pwd("postgres")
+
+
+def test_value():
+    assert "Valid until end of month" == mypwd.get_value("postgres", "note")
+
+
+def test_login_password():
+    login, password = mypwd.get_login_password("postgres")
+    assert "john" == login
+    assert "myPa$$w0rd" == password
+
+
+def test_login_password_note():
+    login, password, note = mypwd.get_values("postgres", ["login", "password", "note"])
+    assert "john" == login
+    assert "myPa$$w0rd" == password
+    assert "Valid until end of month" == note
+
