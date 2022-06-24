ab7bae38d857c0b15e1e6fd98c510deb679ae7a7
Jaroslav Beran <jaroslav.beran@gmail.com>
2022-06-24 23:10:14 +0200

    List of entries from vault

--- mypwd/cli.py
+++ mypwd/cli.py
@@ -10,6 +10,12 @@ def encrypt(args) -> None:
     impl.encrypt_vault(args.email)
 
 
+def list(args):
+    v = impl.load_vault()
+    for k in v.keys():
+        print(k)
+
+
 def main():
     impl.check_if_gpg_is_installed()
 
@@ -27,6 +33,10 @@ def main():
         help='Encrypt vault file with public key identified by e-mail.'
     )
     parser_e.set_defaults(func=encrypt)
+
+    # list
+    parser_l = subparsers.add_parser('list', help='List entries.')
+    parser_l.set_defaults(func=list)
 
     args = parser.parse_args()
     if "func" in args:
