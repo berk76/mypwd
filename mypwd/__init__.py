670edc77970d6a9eb1a19f884adb993769cbea84
Jaroslav Beran <jaroslav.beran@gmail.com>
2026-02-02 14:56:53 +0100

    Version option

--- mypwd/__init__.py
+++ mypwd/__init__.py
@@ -1,5 +1,22 @@
 import mypwd.impl as impl
+import importlib.metadata
+import tomllib  # Python 3.11+, use tomli for older versions
 from mypwd.mypwd_error import MyPwdError
+
+
+def get_version():
+    try:
+        return importlib.metadata.metadata('mywatering-client')['Version']
+    except (importlib.metadata.PackageNotFoundError, KeyError):
+        try:
+            with open("pyproject.toml", "rb") as f:
+                pyproject_data = tomllib.load(f)
+            return pyproject_data["project"]["version"]
+        except Exception:
+            return "unknown"
+
+
+__version__ = get_version()
 
 
 def get_values(entry: str, keys: list) -> list:
